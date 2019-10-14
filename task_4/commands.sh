c1929bcc13921d0d32c5b38ce0fab3a7269f3fbe
m1x0n <mmorozovm@gmail.com>
2019-10-14 20:41:07 +0300

    Updated answers.

--- task_4/commands.sh
+++ task_4/commands.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
-docker build . -t resizer
-
 docker run --name test_resizer \
             -p 8080:80 \
-            -v /home/m1x0n/Pictures:/images:ro \
-            -d resizer
+            -v /path/to/images:/images:ro \
+            -v /path/to/cache/:/resized_images \
+            -d m1x0n/nginx-image-resizer
